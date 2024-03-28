f34f9962207be5ae5e2fb68e71a91de4df2e33e3
Georgi Tangardzhiev <gkyshenko@abv.bg>
2024-03-28 16:53:30 +0200

    new project using webflux

---
+++ online-library-webflux/src/main/java/com/dxc/library/webflux/OnlineLibraryWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.dxc.library.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineLibraryWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineLibraryWebfluxApplication.class, args);
+    }
+
+}
